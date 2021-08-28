ea539f92c9f4884b9650feeccee569443f2e316b
nannany <ymym1990ymym@gmail.com>
2021-08-28 21:56:18 +0900

    main.tf 修正

--- terraform_in_action/1sho/main.tf
+++ terraform_in_action/1sho/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 resource "aws_instance" "helloworld" {
-  ami = "ami-09dd2e08d601bff67"
+  ami = "ami-09ebacdc178ae23b7"
   instance_type = "t2.micro"
   tags = {
     Name = "HelloWorld"
