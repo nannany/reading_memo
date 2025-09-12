b6f2bea47d2fb70152a511d4899be9ab3eded7b9
nannany <ymym1990ymym@gmail.com>
2025-09-12 21:23:01 +0900

    3.1まで

--- ゼロから始めるLean言語入門/2/LeanBook/LeanBook/Logic/PropLogic.lean
+++ ゼロから始めるLean言語入門/2/LeanBook/LeanBook/Logic/PropLogic.lean
@@ -8,3 +8,52 @@ example (P Q : Prop) (h : P → Q) (hp : P) : Q := by
 example (P Q : Prop) (hq : Q) : P → Q := by
   intro hp
   apply hq
+
+
+#eval ¬ True
+
+example (P Q: Prop) (h1 : P → Q) (h2 : Q → P) : P ↔ Q := by
+  constructor
+  · apply h1
+  · apply h2
+
+
+example (P Q : Prop) (hq : Q) : (Q → P) ↔ P := by
+  constructor
+
+  case mp =>
+    intro h
+    exact h hq
+
+  case mpr =>
+    intro hp hq
+    exact hp
+
+
+  example (P Q : Prop): (¬ P ∨ Q) → (P → Q) := by
+    intro h
+    intro hp
+    cases h with
+    | inl hn =>
+      exfalso
+      apply hn
+      exact hp
+    | inr hq =>
+      exact hq
+
+
+  example (P Q : Prop) : ¬ (P ∨ Q) → (¬ P ∧ ¬ Q) := by
+    intro h
+    constructor
+
+    case left =>
+      intro hp
+      apply h
+      left
+      exact hp
+
+    case right =>
+      intro hq
+      apply h
+      right
+      exact hq
